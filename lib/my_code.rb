4060941dab2c7bcc65ddee24a112e4759f223e3b
Max M <maxmcfadden88@gmail.com>
2019-11-16 03:25:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,3 +7,18 @@ def map(array)
   end
   new
 end
+
+def reduce(array, starting_point=nil)
+  if starting_point
+    accum = starting_point
+    i = 0
+  else
+    accum = s[0]
+    i = 1
+  end
+  while i < s.length
+    accum = yield(accum, s[i])
+    i += 1
+  end
+  accum
+end
